2e556a8a25bcef694efc52bafbd13a964277ff6b
Petr Král <pkral2000@gmail.com>
2024-05-12 10:38:19 +0200

    added better commented version

--- kral_svoboda_3.R
+++ kral_svoboda_3.R
@@ -11,7 +11,9 @@ plot(data$Date, tsm, type='l')
 
 # log-returns series
 lr = diff(log(tsm))
-plot(data$Date[-1], lr, type='l')
+plot(data$Date[-1], lr, type='l') # we can observe typical behavior of financial series
+acf(lr) # looks fine
 
 m_arima = auto.arima(lr, allowmean = TRUE)
 summary(m_arima)
+checkresiduals(m_arima) # uncorrelated with p=0.4, heteroscedastic, leptocurtic
